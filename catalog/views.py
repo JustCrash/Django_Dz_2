a27b182b186b02db13a573fbdba76d70d3ba9a58
Artem <baza.a1@bk.ru>
2024-04-24 21:15:44 +0300

    Создал и запустил заполнение

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'catalog/product_list_basic.html', context)
 
 
 def contacts(request):
