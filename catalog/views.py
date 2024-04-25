12da86d8c81f6025a438636deb6d08efd832a381
Artem <baza.a1@bk.ru>
2024-04-25 20:40:57 +0300

    Создан новый контроллер и шаблон для отображения одного товара

--- catalog/views.py
+++ catalog/views.py
@@ -9,6 +9,12 @@ def home(request):
     return render(request, 'catalog/product_list_basic.html', context)
 
 
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {"product": product}
+    return render(request, 'catalog/product_detail.html', context)
+
+
 def contacts(request):
     if request.method == 'POST':
         name = request.POST.get('name')
