53d9c6827a38cbd5dcbcac975a1f590a8cb51668
Artem <baza.a1@bk.ru>
2024-05-02 19:25:28 +0300

    Создал новую модель и применил миграции

--- catalog/models.py
+++ catalog/models.py
@@ -66,3 +66,47 @@ class Product(models.Model):
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
         ordering = ["name", "category", "purchase_price"]
+
+
+class BlogPost(models.Model):
+    title = models.CharField(
+        max_length=255,
+        verbose_name='Заголовок',
+        help_text='Напишите заголовок',
+    )
+    slug = models.CharField(
+        max_length=255,
+        verbose_name='Ссылка',
+        **NULLABLE,
+        help_text='Вставте ссылку',
+    )
+    content = models.TextField(
+        verbose_name='Содержание',
+        help_text='Напишите содержание',
+    )
+    preview = models.ImageField(
+        upload_to='catalog/',
+        verbose_name='Изображение',
+        help_text='Вставте изображение',
+        **NULLABLE,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания',
+        help_text='Укажите дату создания',
+    )
+    publication_sing = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовать',
+    )
+    number_of_views = models.IntegerField(
+        default=0,
+        verbose_name='Количество просмотров',
+    )
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
